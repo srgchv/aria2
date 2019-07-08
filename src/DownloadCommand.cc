6734c64cdcfba68ade1706bcedc814b6acf9bf83
srgchv <srgchv@gmail.com>
2019-07-08 13:11:30 +0300

    Use RETRY instead of ABORT on low downloading speed

--- src/DownloadCommand.cc
+++ src/DownloadCommand.cc
@@ -295,7 +295,7 @@ void DownloadCommand::checkLowestDownloadSpeed() const
      startupIdleTime_) {
     int nowSpeed = peerStat_->calculateDownloadSpeed();
     if(nowSpeed <= lowestDownloadSpeedLimit_) {
-      throw DL_ABORT_EX2(fmt(EX_TOO_SLOW_DOWNLOAD_SPEED,
+      throw DL_RETRY_EX2(fmt(EX_TOO_SLOW_DOWNLOAD_SPEED,
                              nowSpeed,
                              lowestDownloadSpeedLimit_,
                              getRequest()->getHost().c_str()),
